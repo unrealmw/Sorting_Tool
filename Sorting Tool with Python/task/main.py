e3463c0944db2f6b568dfcf11e2840ac4cad27c8
Farrukh Nurmatov <unreal.mw@gmail.com>
2024-11-01 14:43:37 +0500

    Forth Stage Completed

--- Sorting Tool with Python/task/main.py
+++ Sorting Tool with Python/task/main.py
@@ -1,13 +1,14 @@
 import argparse
 
 
-
 class StringSortingTool:
-    def __init__(self):
+    def __init__(self, sort_type="natural"):
+        self.sort_type = sort_type
         self.data = []
         self.max_item = ""
         self.counter = 0
         self.percentage = 0
+        self.count_dict = dict()
 
     def add_data(self):
         while True:
@@ -17,29 +18,47 @@ class StringSortingTool:
             except EOFError:
                 break
 
-    def max(self):
+    def find_max_item(self):
         for item in self.data:
             if len(item) > len(self.max_item):
                 self.max_item = item
 
-    def counting_max(self):
+    def count_max_occurrences(self):
         for item in self.data:
             if len(item) == len(self.max_item):
                 self.counter += 1
 
-    def percentage_counter(self):
-        self.percentage = int(self.counter / len(self.data) * 100)
+    def calculate_percentage(self, counter):
+        percentage = int(counter / len(self.data) * 100)
+        return percentage
 
     def __str__(self):
         return (f"Total lines: {len(self.data)}.\n"
                 f"The longest line:\n"
                 f"{self.max_item}\n"
-                f"({self.counter} time(s), {self.percentage}%).")
+                f"({self.counter} time(s), {self.calculate_percentage(self.counter)}%).")
+
+    def fill_count_dict(self):
+        for item in self.data:
+            if item in self.count_dict.keys():
+                self.count_dict[item] += 1
+            else:
+                self.count_dict[item] = 1
 
     def sorting_result(self):
         self.sort_data()
-        print(f"Total lines: {len(self.data)}.\n"
-              f"Sorted data: {' '.join(self.data)}")
+        self.fill_count_dict()
+        self.count_dict = dict(sorted(self.count_dict.items(), key=lambda x: x[1]))
+        print(f"Total lines: {len(self.data)}.")
+        match self.sort_type:
+            case "natural":
+                print(f"Sorted data:")
+                for line in self.data:
+                    print(line)
+            case "byCount":
+                for k in self.count_dict.keys():
+                    v = self.count_dict[k]
+                    print(f"{k}: {v} time(s), {self.calculate_percentage(v)}%")
 
     def sort_data(self):
         self.data = self._merge_sort(self.data)
@@ -85,19 +104,33 @@ class WordSortingTool(StringSortingTool):
             except EOFError:
                 break
 
+    def fill_count_dict(self):
+        for item in self.data:
+            if item in self.count_dict.keys():
+                self.count_dict[item] += 1
+            else:
+                self.count_dict[item] = 1
+
     def __str__(self):
         return (f"Total words: {len(self.data)}.\n"
-                f"The longest word: {self.max_item} ({self.counter} time(s), {self.percentage}%).")
+                f"The longest word: {self.max_item} ({self.counter} time(s), "
+                f"{self.calculate_percentage(self.counter)}%).")
+
+    def sorting_result(self):
+        self.sort_data()
+        self.fill_count_dict()
+        self.count_dict = dict(sorted(self.count_dict.items(), key=lambda x: x[1]))
+        print(f"Total words: {len(self.data)}.")
+        match self.sort_type:
+            case "natural":
+                print(f"Sorted data: {' '.join(self.data)}")
+            case "byCount":
+                for k in self.count_dict.keys():
+                    v = self.count_dict[k]
+                    print(f"{k}: {v} time(s), {self.calculate_percentage(v)}%")
 
 
-class NumberSortingTool(StringSortingTool):
-    def __init__(self):
-        super().__init__()
-        self.data = []
-        self.max_item = 0
-        self.counter = 0
-        self.percentage = 0
-
+class NumberSortingTool(WordSortingTool):
     def add_data(self):
         while True:
             try:
@@ -106,24 +139,33 @@ class NumberSortingTool(StringSortingTool):
             except EOFError:
                 break
 
-    def max(self):
+    def find_max_item(self):
         for item in self.data:
             if item > self.max_item:
                 self.max_item = item
 
-    def counting_max(self):
+    def count_max_occurrences(self):
         for item in self.data:
             if item == self.max_item:
                 self.counter += 1
 
     def __str__(self):
         return (f"Total numbers: {len(self.data)}.\n"
-                f"The greatest number: {self.max_item} ({self.counter} time(s), {self.percentage}%).")
+                f"The greatest number: {self.max_item} ({self.counter} time(s), "
+                f"{self.calculate_percentage(self.counter)}%).")
 
     def sorting_result(self):
         self.sort_data()
-        print(f"Total numbers: {len(self.data)}.\n"
-              f"Sorted data: {' '.join(map(str, self.data))}")
+        self.fill_count_dict()
+        self.count_dict = dict(sorted(self.count_dict.items(), key=lambda x: x[1]))
+        print(f"Total numbers: {len(self.data)}.")
+        match self.sort_type:
+            case "natural":
+                print(f"Sorted data: {' '.join(map(str, self.data))}")
+            case "byCount":
+                for k in self.count_dict.keys():
+                    v = self.count_dict[k]
+                    print(f"{k}: {v} time(s), {self.calculate_percentage(v)}%")
 
 
 
@@ -131,29 +173,22 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="This program sorts input strings.")
 
     parser.add_argument("-dataType", default="word", choices=["word", "line", "long"])
-    parser.add_argument("-sortIntegers", action="store_true")
+    parser.add_argument("-sortingType", default="natural", choices=["natural", "byCount"])
     args = parser.parse_args()
     mark = args.dataType
-    sort_int = args.sortIntegers
+    sorting_type = args.sortingType
 
-    if sort_int:
-        d = NumberSortingTool()
-        d.add_data()
-        d.sorting_result()
-    else:
-        match mark:
-            case "line":
-                tool = StringSortingTool()
-            case "word":
-                tool = WordSortingTool()
-            case "long":
-                tool = NumberSortingTool()
 
-        tool.add_data()
-        tool.max()
-        tool.counting_max()
-        tool.percentage_counter()
-        print(str(tool))
+    match mark:
+        case "line":
+            tool = StringSortingTool(sort_type=sorting_type)
+        case "word":
+            tool = WordSortingTool(sort_type=sorting_type)
+        case "long":
+            tool = NumberSortingTool(sort_type=sorting_type)
+
+    tool.add_data()
+    tool.sorting_result()
 
 
 
